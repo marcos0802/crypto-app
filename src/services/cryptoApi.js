d4f82f9705362f9af750bb527c9a55ec5aa0192b
root <root@localhost.localdomain>
2021-10-14 19:09:28 +0100

    feat : add crypto api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "b36d841816msh94d8d1287a6bba2p1f7751jsn0f1b9145b37b",
+};
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
